11681e934b32584002e685658b1ff8c9f45b02ea
Michael Guedko <mjgeko@gmail.com>
2022-04-11 17:06:32 -0400

    Move sidebar image to js from html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 //Imports
 import './style.css';
+import SidebarImage from './spaceinvaders-sidebar.png';
+const content = document.getElementById('content');
 
-
-console.log("File loaded & ran successfully");
+function pageLoad(){
+    const element = document.createElement('div');
+    console.log('Loaded');
+    const sidebarL = new Image();
+    const sidebarR = new Image();
+    sidebarL.src = SidebarImage;
+    sidebarR.src = SidebarImage;
+    sidebarL.classList.add('img-left');
+    sidebarR.classList.add('img-right');
+    element.appendChild(sidebarL);
+    element.appendChild(sidebarR);
+    return element;
+}
+content.appendChild(pageLoad());
